03a118ee63e29c6a207a7ce87a0f0690d28d1f62
Mainor Claros <mainor@violetgrey.com>
2015-02-06 12:28:17 -0800

    change taxon search routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Spree::Core::Engine.routes.draw do
-  get "/admin/orders/by-taxons" => "orders#by_taxon_index"
+  get "/by-taxons" => "admin/orders#by_taxon_index", as: 'admin_search_by_taxons'
 end
