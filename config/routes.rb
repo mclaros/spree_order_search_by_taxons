0510df4bf51019f471d9686050c3e7b46b9b7806
Mainor Claros <mainor@violetgrey.com>
2015-02-10 10:14:42 -0800

    change route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Spree::Core::Engine.routes.draw do
-  get "/by-taxons" => "admin/orders#by_taxon_index", as: 'admin_search_by_taxons'
+  namespace :admin do
+    resources :orders do
+      get :by_taxon, on: :collection
+    end
+  end
+
+  # get "/admin/orders/by-taxons", to: "admin/orders#by_taxon_index" #, as: "admin_orders_by_taxons"
+  # get "/by-taxons" => "admin/orders#by_taxon_index", as: 'admin_search_by_taxons'
 end
